aa77afbf5b18fa778c7924133d329e7cc738b961
EstherWaweru <estherwaweru86@gmail.com>
2021-01-26 13:00:27 +0300

    Swapping case of a string

---
+++ String/SwapCase/Swap.py
@@ -0,0 +1,16 @@
+def swap_case(s):
+    new_s=''
+    for i in range(len(s)):
+        if s[i].isupper():
+            new_s+=s[i].lower()
+        elif s[i].islower():
+            new_s+=s[i].upper()
+        else:
+            new_s+=s[i]
+    #s.swapcase()
+    return new_s
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
