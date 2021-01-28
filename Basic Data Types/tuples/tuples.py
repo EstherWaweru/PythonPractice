2871c7d4f6e61e106a03f1f7caf05ce3102771d5
EstherWaweru <estherwaweru86@gmail.com>
2021-01-28 18:43:49 +0300

    Refactor

--- Basic Data Types/tuples/tuples.py
+++ Basic Data Types/tuples/tuples.py
@@ -1,4 +1,34 @@
 #tuples are used in swapping a,b=b,a  also used for keys in dictionaries
+# Task
+# Given an integer, , and space-separated integers as input, create a tuple, , of those integers. Then compute and print the result of
+
+# .
+
+# Note: hash() is one of the functions in the __builtins__ module, so it need not be imported.
+
+# Input Format
+
+# The first line contains an integer,
+# , denoting the number of elements in the tuple.
+# The second line contains space-separated integers describing the elements in tuple
+
+# .
+
+# Output Format
+
+# Print the result of
+
+# .
+
+# Sample Input 0
+
+# 2
+# 1 2
+
+# Sample Output 0
+
+# 3713081631934410656
+
 
 if __name__ == '__main__':
     n = int(input())
